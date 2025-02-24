e25256e46798f623e2d01a811d6161e68dab6b10
rikeda-cloud <longsichitian648@gmail.com>
2025-02-24 14:26:45 +0900

    [update] main.go

--- srcs/cmd/longest-path-solver/main.go
+++ srcs/cmd/longest-path-solver/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	"github.com/rikeda-cloud/longest-path-solver/internal/algorithm"
+	"github.com/rikeda-cloud/longest-path-solver/internal/input"
+	"github.com/rikeda-cloud/longest-path-solver/internal/output"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	graphInuts, err := input.ParseGraphInputs(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	graph, err := input.ConvertGraphInputsToGraph(graphInuts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := algorithm.FindLongestPathByDfs(graph)
+	output.PrintResult(result)
 }
