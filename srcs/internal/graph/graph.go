c55ffaa01d370797385e03bf63ba8f632ab1a749
rikeda-cloud <longsichitian648@gmail.com>
2025-02-24 13:50:52 +0900

    [fix] Graph.adj -> Graph.Adj

--- srcs/internal/graph/graph.go
+++ srcs/internal/graph/graph.go
@@ -10,11 +10,11 @@ type Edge struct {
 }
 
 type Graph struct {
-	adj map[EdgeID][]Edge // adj = "Adjacency List"
+	Adj map[EdgeID][]Edge // adj = "Adjacency List"
 }
 
 func NewGraph() *Graph {
-	return &Graph{adj: make(map[EdgeID][]Edge)}
+	return &Graph{Adj: make(map[EdgeID][]Edge)}
 }
 
 func NewGraphWithEdges(edges map[EdgeID][]Edge) *Graph {
@@ -22,17 +22,17 @@ func NewGraphWithEdges(edges map[EdgeID][]Edge) *Graph {
 	for key, edges := range edges {
 		adj[key] = edges
 	}
-	return &Graph{adj: adj}
+	return &Graph{Adj: adj}
 }
 
 func (g *Graph) Equal(other *Graph) bool {
 	// Compare the number of edges in both graphs
-	if len(g.adj) != len(other.adj) {
+	if len(g.Adj) != len(other.Adj) {
 		return false
 	}
 
-	for key, edges := range g.adj {
-		otherEdges, exists := other.adj[key]
+	for key, edges := range g.Adj {
+		otherEdges, exists := other.Adj[key]
 		// Check if the other graph has the same key and the same number of edges
 		if !exists || len(edges) != len(otherEdges) {
 			return false
@@ -55,14 +55,14 @@ func (g *Graph) AddEdge(id1, id2 EdgeID, distance float64) error {
 
 	// Adds an edge between id1 and id2 with the given distance,
 	// ensuring both directions are stored for the undirected graph.
-	g.adj[id1] = append(g.adj[id1], Edge{To: id2, Distance: distance})
-	g.adj[id2] = append(g.adj[id2], Edge{To: id1, Distance: distance})
+	g.Adj[id1] = append(g.Adj[id1], Edge{To: id2, Distance: distance})
+	g.Adj[id2] = append(g.Adj[id2], Edge{To: id1, Distance: distance})
 
 	return nil
 }
 
 func (g *Graph) edgeExists(id1, id2 EdgeID) bool {
-	for _, edge := range g.adj[id1] {
+	for _, edge := range g.Adj[id1] {
 		if edge.To == id2 {
 			return true
 		}
